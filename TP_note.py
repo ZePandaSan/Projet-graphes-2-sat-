a1950e8a0b4b9f7f1227dea2b00e7b72e35f0a9f
Wassim SAIDANE <72514191+wasshisoka44@users.noreply.github.com>
2020-11-18 18:29:30 +0100

    Add files via upload

--- TP_note.py
+++ TP_note.py
@@ -1,8 +1,9 @@
-print("Wassim SAIDANE et Aurélien Authier" )
+print("Wassim SAIDANE et Aurélien Authier")
 
 
-F="1|-2&3|4&-2|-3&4|-5&2|-5"
-dic={}
+F = "1|-2&3|4&-2|-3&4|-5&2|-5"
+G = "0|2&0|-3&1|-3&1|-4&2|-4&0|-5&1|-5&2|-5&3|6&4|6&5|6"
+dic = {}
 dic[1] = True
 dic[2] = False
 dic[3] = True
@@ -16,20 +17,24 @@ dic[-4] = not(dic[4])
 dic[-5] = not(dic[5])
 dic[-6] = not(dic[6])
 
+
 def clauses_liste(F):
     return F.split("&")
 
-def variables_par_clause(l1,l2):
+
+def variables_par_clause(l1, l2):
     for i in range(len(l2)):
         l1.append(l2[i].split("|"))
 
-def eval_clauses(d1,d2,l):
+
+def eval_clauses(d1, d2, l):
     for i in range(len(l)):
         v1 = int(l[i][0])
         v2 = int(l[i][1])
         d2[tuple(l[i])] = (d1[v1] or d1[v2])
 
-def eval_2_sat(f,d1):
+
+def eval_2_sat(f, d1):
     #---------------------------------------------------------------------------------------------------------
     #| Le code ne prend pas en comple les formules qui ne sont pas rentrées en suivant notre sémentique"    #|
     #| Pas d'espace !                                                                                       #|
@@ -37,34 +42,54 @@ def eval_2_sat(f,d1):
     #| Les parenthés sont sous-entendu caractères avant le "&"                                              #|
     #| Les variables sont séparées par "|" (ou)                                                             #|
     #| Les formules 2-sat fonctionnent                                                                      #|
-    #| Les formules 3-sat, 4-sat, ... devraient fonctionner                                                 #|
     #---------------------------------------------------------------------------------------------------------
     print(f"Affectation : {dic}")
     clauses = clauses_liste(f)
     print(f"Clauses : {clauses}")
     result = True
-    clause=[]
-    variables_par_clause(clause,clauses)
+    clause = []
+    variables_par_clause(clause, clauses)
     print(f"Variables par clause : {clause}")
-    d2={}
-    eval_clauses(d1,d2,clause)
+    d2 = {}
+    eval_clauses(d1, d2, clause)
     print(f"Evaluations par clause : {d2}")
     for value in d2:
-        result=(result and d2[value])
+        result = (result and d2[value])
     return result
 
 
-def formula_2_graph(F, d):
-    return " "
+def formula_2_graph(F):
+    clauses = clauses_liste(F)
+    print(f"Clauses : {clauses}")
+    clause = []
+    variables_par_clause(clause, clauses)
+    print(f"Variables par clause : {clause}")
+    g = DiGraph()
+    for var in clause:
+        g.add_vertices([int(var[0]), int(var[1])])
+    sommets = g.vertices()
+    for sommet in sommets:
+        g.add_vertices([sommet*-1])
+    sommets = g.vertices()
+    print(f"Listes des sommets : {sommets}")
+    for i in range(0, len(clause)):
+        clause[i] = [int(clause[i][0]), int(clause[i][1])]
+    for var in clause:
+        g.add_edges([(var[0]*-1, var[1])])
+        g.add_edges([(var[1]*-1, var[0])])
+    return g
+
 
 print("Exercice 3 : ")
 print(" ")
 print(f"F={F}")
 if eval_2_sat(F, dic):
     print("F est satisfaisable")
-else :
+else:
     print("F n'est pas satisfaisable")
 print(" ")
 print(" ")
 print("Exercice 4 : ")
-
+print(f"F={F}")
+g = formula_2_graph(F)
+g.show()
